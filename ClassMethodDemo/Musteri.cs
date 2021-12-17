2b9aacb620d2fa593e69c2dd8f9d98f25ad54aae
mustafw <mustafabasarn3@gmail.com>
2021-12-17 22:12:10 +0300

    Add project files.

---
+++ ClassMethodDemo/Musteri.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDemo
+{
+    class MusteriYonetim
+    {
+        static Musteri[] musteriler = new Musteri[] {};
+        private static bool _filterCondition_ID0(Musteri musteri, int id = -1, string ad = null, string soyad = null, int cinsiyet = -1, long bakiye = -1)
+        {
+            return ((id >= 0 && musteri.id == id) || (ad != null && musteri.ad == ad) || (soyad != null && musteri.soyad == soyad) || (cinsiyet >= 0 && musteri.cinsiyet == cinsiyet) || (bakiye >= 0 && musteri.bakiye == bakiye));
+        }
+        public static void MusteriEkle(string ad, string soyad, int cinsiyet, long bakiye)
+        {
+            Musteri musteriBellek = new Musteri();
+            musteriBellek.id = musteriler.Last().id + 1;
+            musteriBellek.ad = ad;
+            musteriBellek.soyad = soyad;
+            musteriBellek.cinsiyet = cinsiyet;
+            musteriBellek.bakiye = bakiye;
+            musteriler.Append(musteriBellek);
+        }
+        public static void MusteriBul(int id = -1, string ad = null, string soyad = null, int cinsiyet = -1, long bakiye = -1)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                if (_filterCondition_ID0(musteri, id, ad, soyad, cinsiyet, bakiye))
+                {
+                    Console.WriteLine(">>> Musteri ID         >>> " + musteri.id);
+                    Console.WriteLine(">>> Musteri AD         >>> " + musteri.ad);
+                    Console.WriteLine(">>> Musteri SOYAD      >>> " + musteri.soyad);
+                    Console.WriteLine(">>> Musteri CINSIYET   >>> " + musteri.cinsiyet);
+                    Console.WriteLine(">>> Musteri BAKIYE     >>> " + musteri.bakiye);
+                }
+            }
+        }
+        public static void MusteriListele(long limit = 999999999)
+        {
+            for (int i = 0, i2 = 0; i < musteriler.Length && i2 < limit; i++, i2++)
+            {
+                Musteri musteri = musteriler[i];
+                Console.WriteLine((i!=0?"\n\n":"")+">>> "+i);
+                Console.WriteLine(">>> Musteri ID         >>> " + musteri.id);
+                Console.WriteLine(">>> Musteri AD         >>> " + musteri.ad);
+                Console.WriteLine(">>> Musteri SOYAD      >>> " + musteri.soyad);
+                Console.WriteLine(">>> Musteri CINSIYET   >>> " + musteri.cinsiyet);
+                Console.WriteLine(">>> Musteri BAKIYE     >>> " + musteri.bakiye);
+            }
+            Console.WriteLine(musteriler.Length<1 ? "Kayıtlı kullanıcı bulunmamaktadır.":"");
+        }
+        public static void MusteriSil(int id = -1, string ad = null, string soyad = null, int cinsiyet = -1, long bakiye = -1)
+        {
+            Musteri[] musterilerRev = new Musteri[] { };
+            foreach (Musteri musteri in musteriler)
+            {
+                if (!_filterCondition_ID0(musteri, id, ad, soyad, cinsiyet, bakiye))
+                {
+                    musterilerRev.Append(musteri);
+                }
+            }
+            musteriler = musterilerRev;
+        }
+    }
+    class Musteri
+    {
+        public int id { get; set; }
+        public string ad { get; set; }
+        public string soyad { get; set; }
+        public int cinsiyet { get; set; }
+        public long bakiye { get; set; }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ClassMethodDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("Eylemler:\n0  -  Müşterileri listele\n1  -  Müşteri sil\n2  -  Müşteri güncelle\n3  -  Müşteri ekle\nEylem seçiniz: ");
+                switch (Console.ReadLine())
+                {
+                    case "0":
+                        Console.Write("Eylemler:\n0  -  Herkesi listele\n1  -   Filtreleyerek listele");
+                        switch (Console.ReadLine())
+                        {
+                            case "0":
+                                MusteriYonetim.MusteriListele();
+                                break;
+                            case "1":
+                                Console.Write("Filtre ID(Boş bırakabilirsiniz): ");
+                                string strId = Console.ReadLine();
+                                Console.Write("Filtre AD(Boş bırakabilirsiniz): ");
+                                string ad = Console.ReadLine();
+                                Console.Write("Filtre SOYAD(Boş bırakabilirsiniz): ");
+                                string soyad = Console.ReadLine();
+                                Console.Write("Filtre CINSIYET(Boş bırakabilirsiniz): ");
+                                string strCinsiyet= Console.ReadLine();
+                                Console.Write("Filtre BAKIYE(Boş bırakabilirsiniz): ");
+                                string strBakiye = Console.ReadLine();
+                                int id = strId!=""?int.Parse(strId):-1, cinsiyet = strCinsiyet!=""?int.Parse(strCinsiyet):-1, bakiye = strBakiye!=""?int.Parse(strBakiye):-1;
+                                ad = ad == "" ? null : ad;
+                                soyad = soyad == "" ? null : soyad;
+                                MusteriYonetim.MusteriBul(id, ad, soyad, cinsiyet, bakiye);
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case "1":
+
+                        break;
+                    default:
+                        Console.WriteLine("\nHatalı bilgi girdiniz! Tekrar deneyiniz.");
+                        break;
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
